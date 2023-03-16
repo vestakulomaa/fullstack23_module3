8f9c0e136844d38d3531622a2331403559ca1d16
Vesta K <vesta.kulomaa@gmail.com>
2023-03-16 13:04:25 +0200

    add build folder

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body)
